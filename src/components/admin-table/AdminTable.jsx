b9c8b8440fba3da97f9421b4fb2041ed20b23012
Thai ne <thailevinh001@192.168.101.48>
2023-10-07 21:13:48 +0700

    fix table

--- src/components/admin-table/AdminTable.jsx
+++ src/components/admin-table/AdminTable.jsx
@@ -24,8 +24,8 @@ const columns = [
     render: (_, { tags }) => (
       <>
         {tags.map((tag) => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
+          let color = tag.length > 6 ? 'active' : 'green';
+          if (tag === 'inactive') {
             color = 'volcano';
           }
           return (
@@ -54,22 +54,51 @@ const data = [
     name: 'John Brown',
     age: 32,
     address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
+    tags: ['active'],
   },
   {
     key: '2',
     name: 'Jim Green',
     age: 42,
     address: 'London No. 1 Lake Park',
-    tags: ['loser'],
+    tags: ['active'],
   },
   {
     key: '3',
     name: 'Joe Black',
     age: 32,
     address: 'Sydney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
+    tags: ['active'],
   },
+  {
+    key: '4',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    tags: ['active'],
+  },
+  {
+    key: '5',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    tags: ['active'],
+  },
+  {
+    key: '6',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    tags: ['active'],
+  },
+  {
+    key: '7',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    tags: ['inactive'],
+  },
+
 ];
 const UserTable = () => <Table columns={columns} dataSource={data} />;
 export default UserTable;
