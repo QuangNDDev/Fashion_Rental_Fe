dcd21a824918c2e9b9de1249bbe0d04ae3e2838d
Quang Nguyen <dangquangdev2k@gmail.com>
2023-10-11 01:10:04 +0700

    fix conflict

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Admin from './pages/admin/Admin';
-import Staff from './pages/staff/Staff';
-import LoginForm from './pages/admin-login/Admin-Login';
-import PrivateRoute from './components/protect-page/Protect-Pages';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Admin from "./pages/admin/Admin";
+import Staff from "./pages/staff/Staff";
+import LoginForm from "./pages/admin-login/admin-login";
 
 function App() {
   return (
     <Routes>
-      <Route path="" element={<LoginForm />} />
+      <Route path="/" element={<LoginForm />} />
       <Route path="admin" element={<Admin />} />
       <Route path="staff" element={<Staff />} />
-      <Route path="login" element={<LoginForm />} />
-      {/* Protect page admin */}
-      <Route element={<PrivateRoute />}>
-        <Route path="admin" element={<Admin />} />
-      </Route>
+      <Route path="admin/login" element={<LoginForm />} />
     </Routes>
   );
 }
