3c2d9a0c5c5125c0919fd9b670cac6c6131a9d53
Quang Nguyen <dangquangdev2k@gmail.com>
2023-10-11 20:36:48 +0700

    update status and modal

--- src/components/Card/CardDetail.jsx
+++ src/components/Card/CardDetail.jsx
@@ -1,18 +1,51 @@
 import React, { useState } from "react";
 import { EditOutlined, SettingOutlined } from "@ant-design/icons";
-import { Avatar, Card, Form, Modal, Button } from "antd";
+import { Avatar, Card, Form, Modal, Button, Result } from "antd";
 import MuntilImage from "../Mutil-Image";
 import { Input } from "antd";
-import InputNumber from "rc-input-number";
+import { Tag } from "antd";
+
 const { Meta } = Card;
 
 const CardDetail = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [formData, setFormData] = useState(null);
   const [form] = Form.useForm();
+  const [cardData, setCardData] = useState({
+    avatar: "https://xsgames.co/randomusers/avatar.php?g=pixel",
+    product_Name: "Louis Vuitton", // Thay đổi title
+    productOwner_Name: "Nguyễn Đăng Quang", // Thay đổi description
+    status: "Đã duyệt",
+    invoice_Code: "ACCCAAA",
+    address: "Vinhome grandpark Quận 9",
+    phone_Number: "0822833799",
+    imgProduct:
+      "https://vn.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-t%C3%BAi-onthego-mm-monogram-t%C3%BAi--M45321_PM2_Front%20view.jpg",
+  });
+
+  const tagColor = {
+    Active: "green",
+    InActive: "red",
+  };
+
+  const tagValue = {
+    Active: "green",
+    InActive: "red",
+  };
 
   const showModal = () => {
     setIsModalVisible(true);
+    // Cập nhật dữ liệu cho Modal dựa trên thẻ hiện tại
+    form.setFieldsValue({
+      productName: cardData.product_Name,
+      ownerName: cardData.productOwner_Name,
+      invoiceCode: cardData.invoice_Code,
+      address: cardData.address,
+      status: cardData.status,
+      phoneNumber: cardData.phone_Number,
+      imgProduct: cardData.imgProduct,
+      // Các trường dữ liệu khác tương tự
+    });
   };
 
   const handleCancel = () => {
@@ -38,7 +71,7 @@ const CardDetail = () => {
         cover={
           <img
             alt="example"
-            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+            src="https://vn.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-t%C3%BAi-onthego-mm-monogram-t%C3%BAi--M45321_PM2_Front%20view.jpg"
           />
         }
         actions={[
@@ -47,11 +80,30 @@ const CardDetail = () => {
         ]}
       >
         <Meta
-          avatar={
-            <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+          avatar={<Avatar src={cardData.avatar} />}
+          title={cardData.product_Name} /* Sử dụng "Tên sản phẩm" */
+          description={
+            <>
+              <div>{cardData.productOwner_Name}</div>
+              <div>
+                {cardData.status === "Đã duyệt" && (
+                  <Tag style={{ textAlign: "center" }} color="green">
+                    {cardData.status}
+                  </Tag>
+                )}
+                {cardData.status === "Đang chờ xử lý" && (
+                  <Tag style={{ textAlign: "center" }} color="yellow">
+                    {cardData.status}
+                  </Tag>
+                )}
+                {cardData.status === "Đã từ chối" && (
+                  <Tag style={{ textAlign: "center" }} color="red">
+                    {cardData.status}
+                  </Tag>
+                )}
+              </div>
+            </>
           }
-          title="Card title"
-          description="This is the description"
         />
       </Card>
       <Modal
@@ -68,7 +120,7 @@ const CardDetail = () => {
         >
           <Form.Item
             label="Tên sản phẩm"
-            name="productName"
+            name="productName" //lấy value của cái name gán lên cái setFormValue
             rules={[{ required: true, message: "Vui lòng nhập tên sản phẩm!" }]}
           >
             <Input style={{ width: "300px", marginLeft: "13px" }} />
@@ -112,13 +164,14 @@ const CardDetail = () => {
 
           <Form.Item
             label="Hình ảnh sản phẩm"
-            name="phoneNumber"
-            rules={[
-              { required: true, message: "Vui lòng nhập số điện thoại!" },
-            ]}
+            name="imgProduct"
+            rules={[{ required: true, message: "" }]}
           >
-            <br></br>
-            <MuntilImage />
+            <img
+              src={cardData.imgProduct}
+              alt="Product"
+              style={{ maxWidth: "100%" }}
+            />
           </Form.Item>
 
           <Form.Item style={{ textAlign: "center" }}>
